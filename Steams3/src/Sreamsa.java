14dd08f13c3123991c83482f467f975a49c9616b
kesa gaurav <kesagaurav@gmail.com>
2021-08-14 10:19:30 +0530

    playerapp

---
+++ Steams3/src/Sreamsa.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sreamsa {
+
+	public static void main(String[] args) {
+		List<Integer> li=new ArrayList<>();
+		li.add(1);
+		li.add(2);
+		li.add(3);
+		li.add(4);
+		li.add(5);
+		
+	}
+	
+}
